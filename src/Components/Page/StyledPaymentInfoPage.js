669fbb296674370e7e800ec1156e7118b25dd8c1
sese98 <river_0@naver.com>
2023-10-19 15:42:22 +0900

    결제안내수정, 글쓰기 이미지핸들러 보드키 수정

--- src/Components/Page/StyledPaymentInfoPage.js
+++ src/Components/Page/StyledPaymentInfoPage.js
@@ -2,16 +2,17 @@ import styled from 'styled-components';
 
 export const Wrap = styled.div`
   /* background: pink; */
-  padding: 190px 0 500px;
+  padding: 190px 0 700px;
   width: 100%;
   height: 1200px; //임시
   color: #888;
   *{box-sizing: border-box; margin: 0; padding: 0;}
+  p.sub-title{text-align: center; font-size: 32px; color: #555; font-weight: 500; margin-bottom: 50px;}
 `
 export const TitleBox = styled.h2`
   text-align: center;
   color: #333;
-  font-size: 36px;
+  font-size: 40px;
   margin-bottom: 100px;
 `
 export const PriceInfo = styled.section`
@@ -34,19 +35,22 @@ export const PriceCard = styled.div`
   justify-content: space-evenly;
   padding: 50px 0 100px;
   color: #555;
-  .price {font-size: 26px; margin-top: 15px; font-weight: 500;}
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.05);
+  .price {font-size: 26px; margin-top: 15px; font-weight: 500; color: #000;}
   .surtax {font-size: 12px;}
   .while {margin: 10px 0 30px; font-size: 14px;}
+  .while span {color: #3fa9f5; font-weight: bold;}
 `
 export const Container = styled.section`
   /* background: palegreen; */
   min-width: 700px;
   max-width: 950px;
-  height: 900px; //임시
-  margin: 100px auto;
+  height: 950px; //임시
+  margin: 200px auto;
   display: flex;
   flex-direction: column;
   justify-content: space-around;
+  
 `
 export const Box = styled.div`
   background: ${(props) => props.background};
@@ -55,21 +59,32 @@ export const Box = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  padding: 0 50px;
+  /* border: 1px solid #ddd;
+  border-radius: 50px; */
+  
+  .icon {color: #afdaf8; transition: color .3s;}
+  &:hover {
+    .icon {color: #87cafa;}
+  }
 `
 
-export const Line = styled.p`
+export const Line = styled.div`
   width: 140px;
-  height: 2px;
+  height: 1px;
   display: inline-block;
   background: #ddd;
 `
 export const TextBox = styled.div`
   /* background: skyblue; */
-  width: 500px;
+  width: 460px;
   height: 150px;
   display: flex;
   align-items: center;
-  font-size: 20px;
+  font-size: 18px;
   font-weight: 300;
   line-height: 1.4;
+  span.bold {font-weight: 500; font-size: 22px; color: #555; margin-bottom: 10px; display: block;}
+  .write {color: #75c1f7; font-weight: bold; transition: color .2s; cursor: pointer;}
+  .write:hover {color: #3fa9f5;}
 `
